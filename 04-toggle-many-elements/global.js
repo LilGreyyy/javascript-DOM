3c127f18e314b2c0fcd5615393f60b362cc30343
LilGreyyy <ilonagreyyy@gmail.com>
2021-03-16 15:23:59 +0200

    4

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,14 @@
-// Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const content_toggle = document.getElementsByClassName("toggle_me");
+    const button_toggle = document.getElementById("button");
+
+    button_toggle.addEventListener('click', function(){
+        Array.from(content_toggle).forEach((elements) => {
+            if(elements.style.display == 'flex') {
+                elements.style.display = 'none'
+            }else{
+                elements.style.display = 'flex'
+            }
+        });
+    });
+});
